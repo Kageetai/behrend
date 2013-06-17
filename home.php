424a803eaf2ff08273fc2295c90e04287e831c09
Kageetai <kageetai@gmail.com>
2013-06-17 22:37:20 +0200

    configured home page, deleted index.html from zurb-foundation

--- home.php
+++ home.php
@@ -1,8 +1,4 @@
-<?php
-if (!defined('IN_GS')) {
-    die('you cannot load this page directly.');
-}
-?>
+<?php if (!defined('IN_GS')) { die('you cannot load this page directly.'); } ?>
 
 <!DOCTYPE html>
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
@@ -20,41 +16,20 @@ if (!defined('IN_GS')) {
 </head>
 <body id="<?php get_page_slug(); ?>">
 
-<div id="header">
-    <div class="headerbox">
-        <a class="headertext" href="<?php get_site_url(); ?>">
-            <p>Dietmar Overhage</p>
-
-            <p class="subheader">Malermeisterbetrieb</p>
-        </a>
-        <div class="headerlogo"></div>
-    </div>
-</div>
-
 <div id="wrapper">
 
-    <div id="logo">
-        <ul id="nav">
-            <li><a href="<?php get_site_url(); ?>">Start</a></li>
-            <?php get_navigation(return_page_slug()); ?>
-        </ul>
-        <div class="address">
-            <?php get_component("address"); ?>
-        </div>
+    <div class="header index">
+        <h1 class="logo">Zahnarztpraxis Wolfgang Behrend</h1>
     </div>
 
     <section id="content">
-        <article class="page-text">
-            <?php get_page_content(); ?>
-        </article>
+        <div class="adressblock">
+            <?php get_component('adress2'); ?>
+        </div>
+        <div class="adressblock">
+            <?php get_component('adress1'); ?>
+        </div>
     </section>
-
-    <section id="right">
-        <?php //get_component('slides'); ?>
-        <?php include('slides.inc.php'); ?>
-    </section>
-
-    <!--	<div class="clear"></div>-->
 
     <?php get_footer(); ?>
 
@@ -64,8 +39,8 @@ if (!defined('IN_GS')) {
     <div class="webmaster">Webmaster <a href="http://Kageetai.net">Kageetai.net</a></div>
 </div>
 
-<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>-->
-<script src="<?php get_theme_url(); ?>/js/jquery-1.9.1.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<!--<script src="--><?php //get_theme_url(); ?><!--/js/jquery-1.9.1.min.js"></script>-->
 <script src="<?php get_theme_url(); ?>/js/foundation/foundation.js"></script>
 <!--<script src="js/foundation/foundation.alerts.js"></script>-->
 <!--<script src="js/foundation/foundation.clearing.js"></script>-->
@@ -81,19 +56,19 @@ if (!defined('IN_GS')) {
 <!--<script src="js/foundation/foundation.tooltips.js"></script>-->
 <!--<script src="js/foundation/foundation.topbar.js"></script>-->
 <script src="<?php get_theme_url(); ?>/js/jquery.cycle.lite.js" type="text/javascript" charset="utf-8"></script>
-<script>
-    $(document).foundation();
-//    $('.headerlogo').click(function() {
-//        $('.headertext').toggleClass('clicked');
-//        $('.headertext > p').fadeToggle();
-    $('#slides').cycle({
-        fit: 1,
-        nowrap: 1,
-        random: 1,
-        speed: 5000,
-        timeout: 5000
-    });
-</script>
+<!--<script>-->
+<!--    $(document).foundation();-->
+<!--//    $('.headerlogo').click(function() {-->
+<!--//        $('.headertext').toggleClass('clicked');-->
+<!--//        $('.headertext > p').fadeToggle();-->
+<!--    $('#slides').cycle({-->
+<!--        fit: 1,-->
+<!--        nowrap: 1,-->
+<!--        random: 1,-->
+<!--        speed: 5000,-->
+<!--        timeout: 5000-->
+<!--    });-->
+<!--</script>-->
 
 </body>
 </html>
