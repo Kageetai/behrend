683b7b0aedede5155342e22eb4edb42b4ac4a6cc
Kageetai <kageetai@gmail.com>
2013-07-09 14:38:58 +0200

    slides not with background but real IMGs

--- slides.inc.php
+++ slides.inc.php
@@ -1,8 +1,12 @@
 <div id="slides">
 	<?php
-	$dir = 'data/uploads/slides/'.get_page_slug(false).'/';
+//	$dir = 'data/uploads/slides/'.get_page_slug(false).'/';
+	$dir = 'data/uploads/slides/';
 	$files = glob(preg_replace('/(\*|\?|\[)/', '[$1]', $dir).'*.jpg');
 	foreach ($files as $filename) { ?>
-	    <div class="slide" style="background-image: url(<?php echo get_site_url(false).$filename; ?>); background-size: cover; background-position: center;">&nbsp;</div>
+<!--	    <div class="slide" style="background-image: url(--><?php //echo get_site_url(false).$filename; ?><!--); background-size: cover; background-position: center;">&nbsp;</div>-->
+	    <div class="slide">
+            <img src="<?php echo get_site_url(false).$filename; ?>" alt="slide" />
+	    </div>
 	<?php } ?>
 </div>
