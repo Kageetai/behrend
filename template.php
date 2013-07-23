b318f6f60a043581032c7f580f646e6a8910fad0
Kageetai <kageetai@gmail.com>
2013-07-23 16:41:47 +0200

    inserted code from home.php into template.php

--- template.php
+++ template.php
@@ -1,12 +1,12 @@
-<?php
-if (!defined('IN_GS')) {
+<?php if (!defined('IN_GS')) {
     die('you cannot load this page directly.');
-}
-?>
+} ?>
 
 <!DOCTYPE html>
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang="de"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="de"> <!--<![endif]-->
 
 <head>
     <title><?php get_site_name(); ?> - <?php get_page_clean_title(); ?></title>
@@ -14,86 +14,71 @@ if (!defined('IN_GS')) {
     <meta name="robots" content="index, follow"/>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width"/>
-    <link href="<?php get_theme_url(); ?>/css/app.css" media="screen, projector, print" rel="stylesheet" type="text/css"/>
+    <link href="<?php get_theme_url(); ?>/css/app.css" media="screen, projector, print" rel="stylesheet"
+          type="text/css"/>
     <script src="<?php get_theme_url(); ?>/js/vendor/custom.modernizr.js"></script>
 
 </head>
 <body id="<?php get_page_slug(); ?>">
 
-<div id="header">
-    <div class="headerbox">
-        <a class="headertext" href="<?php get_site_url(); ?>">
-            <p>Dietmar Overhage</p>
-
-            <p class="subheader">Malermeisterbetrieb</p>
-        </a>
-        <div class="headerlogo"></div>
-    </div>
-</div>
-
 <div id="wrapper">
 
-    <div id="logo">
-        <ul id="nav">
-            <li><a href="<?php get_site_url(); ?>">Start</a></li>
-            <?php get_navigation(return_page_slug()); ?>
-        </ul>
-        <div class="address">
-            <?php get_component("address"); ?>
+    <section class="header">
+        <div class="logoblock">
+            <h1 class="logo">Zahnarztpraxis Wolfgang Behrend</h1>
         </div>
-    </div>
 
-    <section id="content">
-        <article class="page-text">
-            <?php get_page_content(); ?>
-        </article>
+        <ul class="navcolumn">
+            <?php get_navigation(true); ?>
+        </ul>
     </section>
 
-    <section id="right">
-        <?php //get_component('slides'); ?>
-        <?php include('slides.inc.php'); ?>
+    <section class="mainrow addr">
+        <div class="addresscolumn">
+            <div class="address">
+                <?php get_component('address1'); ?>
+            </div>
+        </div>
+        <div class="addresscolumn">
+            <div class="address">
+                <?php get_component('address2'); ?>
+            </div>
+        </div>
     </section>
 
-    <!--	<div class="clear"></div>-->
+    <section class="mainrow sdl">
+        <div class='gallerycolumn'>
+            <?php include('slides.inc.php'); ?>
+        </div>
+    </section>
 
-    <?php get_footer(); ?>
+    <section class="mainrow cnt">
+        <div class="contactcolumn">
+            <div class="openingtimes">
+                <?php get_component('sprechzeiten'); ?>
+            </div>
+            <div class="contact">
+                <?php get_component('contact'); ?>
+            </div>
+        </div>
+        <div class="contentcolumn">
+            <div class="content">
+                <?php get_page_content(); ?>
+            </div>
+        </div>
+    </section>
 
+    <section class="footer">
+        <div class="webmaster">Webmaster <a href="http://Kageetai.net">Kageetai.net</a></div>
+    </section>
 </div>
 
-<div class="footer">
-    <div class="webmaster">Webmaster <a href="http://Kageetai.net">Kageetai.net</a></div>
-</div>
+<?php get_footer(); ?>
 
 <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>-->
 <script src="<?php get_theme_url(); ?>/js/jquery-1.9.1.min.js"></script>
 <script src="<?php get_theme_url(); ?>/js/foundation/foundation.js"></script>
-<!--<script src="js/foundation/foundation.alerts.js"></script>-->
-<!--<script src="js/foundation/foundation.clearing.js"></script>-->
-<!--<script src="js/foundation/foundation.cookie.js"></script>-->
-<!--<script src="js/foundation/foundation.dropdown.js"></script>-->
-<!--<script src="js/foundation/foundation.forms.js"></script>-->
-<!--<script src="js/foundation/foundation.joyride.js"></script>-->
-<!--<script src="js/foundation/foundation.magellan.js"></script>-->
-<!--<script src="js/foundation/foundation.orbit.js"></script>-->
-<!--<script src="js/foundation/foundation.placeholder.js"></script>-->
-<!--<script src="js/foundation/foundation.reveal.js"></script>-->
-<!--<script src="js/foundation/foundation.section.js"></script>-->
-<!--<script src="js/foundation/foundation.tooltips.js"></script>-->
-<!--<script src="js/foundation/foundation.topbar.js"></script>-->
-<script src="<?php get_theme_url(); ?>/js/jquery.cycle.lite.js" type="text/javascript" charset="utf-8"></script>
-<script>
-    $(document).foundation();
-//    $('.headerlogo').click(function() {
-//        $('.headertext').toggleClass('clicked');
-//        $('.headertext > p').fadeToggle();
-    $('#slides').cycle({
-        fit: 1,
-        nowrap: 1,
-        random: 1,
-        speed: 5000,
-        timeout: 5000
-    });
-</script>
+<!--<script src="--><?php //get_theme_url(); ?><!--/js/jquery.cycle.lite.js" type="text/javascript" charset="utf-8"></script>-->
 
 </body>
 </html>
