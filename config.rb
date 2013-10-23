e97743912e6bbb8621609a1b4f0eb8c792c43df1
Kageetai <kageetai@gmail.com>
2013-10-23 15:25:14 +0200

    changed from sass syntax to scss

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ require "zurb-foundation"
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
 
@@ -21,6 +21,6 @@ sass_options = {:debug_info=>true}
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
-preferred_syntax = :sass
+preferred_syntax = :scss
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
